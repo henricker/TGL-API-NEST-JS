a3278e8d42b6ae67fc0e9d057ad28e4bd8d72761
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-10-13 18:14:44 -0300

    chore: add graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { GameModule } from './game/game.module';
 import { BetModule } from './bet/bet.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 
 @Module({
-  imports: [UserModule, GameModule, BetModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    UserModule,
+    GameModule,
+    BetModule,
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
 })
 export class AppModule {}
