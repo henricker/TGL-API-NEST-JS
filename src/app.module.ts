b7e7d04997717ee5a7bed2ad4e64cd8a5d0b965f
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-10-13 09:17:20 -0300

    refactor: remove prismaservice of App module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaService } from './prisma.service';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [UserModule],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService],
 })
 export class AppModule {}
