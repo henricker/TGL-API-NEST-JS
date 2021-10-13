51d90336663e27a5b5df1c20954c081eaaaa2ce4
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-10-13 18:19:35 -0300

    fix: add PrismaService in provider

--- src/bet/bet.module.ts
+++ src/bet/bet.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
 import { BetService } from './bet.service';
 
 @Module({
-  providers: [BetService],
+  providers: [BetService, PrismaService],
 })
 export class BetModule {}
