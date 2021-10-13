52402ccbca05e96245c227c8c6671505baed6278
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-10-13 08:37:11 -0300

    refactor: add prismaService in provider of App

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,7 @@
 import { Module } from '@nestjs/common';
-import { PrismaService } from 'src/prisma.service';
 import { UserService } from './user.service';
 
 @Module({
-  providers: [UserService, PrismaService],
+  providers: [UserService],
 })
 export class UserModule {}
