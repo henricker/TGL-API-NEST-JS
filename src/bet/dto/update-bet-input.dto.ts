94423fc378a7b62e0f01c63b3d67aad50a732a83
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-10-14 19:01:03 -0300

    fix: change type numbers to array int not optional

--- src/bet/dto/update-bet-input.dto.ts
+++ src/bet/dto/update-bet-input.dto.ts
@@ -1,10 +1,9 @@
-import { Field, InputType } from '@nestjs/graphql';
-import { IsOptional, IsString } from 'class-validator';
+import { Field, InputType, Int } from '@nestjs/graphql';
+import { IsArray } from 'class-validator';
 
 @InputType()
 export class UpdateBetInputDTO {
-  @Field(() => String)
-  @IsOptional()
-  @IsString()
-  numbers?: string;
+  @Field(() => [Int])
+  @IsArray()
+  numbers: number[];
 }
