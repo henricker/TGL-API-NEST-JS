b16dc7f76f3ecc3e614b5dedef9b4b5b98266f89
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-10-14 17:55:25 -0300

    feat: add methods following business logic

--- src/bet/bet.service.ts
+++ src/bet/bet.service.ts
@@ -3,6 +3,7 @@ import { Bet } from '.prisma/client';
 import BaseService from '../shared/base.service';
 import { CreateBetInputDTO } from './dto/create-bet-input.dto';
 import { PrismaService } from '../prisma.service';
+import { UpdateBetInputDTO } from './dto/update-bet-input.dto';
 
 @Injectable()
 export class BetService extends BaseService<Bet> {
@@ -10,7 +11,7 @@ export class BetService extends BaseService<Bet> {
     super(prisma);
   }
 
-  public async create(data: CreateBetInputDTO): Promise<Bet> {
+  public async createBet(data: CreateBetInputDTO): Promise<Bet> {
     const existsUser = !!(await this.prisma.user.findUnique({
       where: { id: data.userId },
     }));
@@ -23,8 +24,62 @@ export class BetService extends BaseService<Bet> {
 
     if (!existsGame) throw new NotFoundException('error: game not found');
 
-    const bet = await this.prisma.bet.create({ data });
+    const bet = await this.prisma.bet.create({
+      data: {
+        ...data,
+        numbers: data.numbers.join(' '),
+      },
+      include: { user: true, game: true },
+    });
+
+    await this.prisma.user.update({
+      where: { id: data.userId },
+      data: { lastBet: new Date() },
+    });
 
     return bet;
   }
+
+  public async show(
+    userId: number,
+    betId: number,
+    options?: showOptions,
+  ): Promise<Bet> {
+    const bet = await this.prisma.bet.findFirst({
+      where: { userId, id: betId },
+      include: options?.include,
+    });
+
+    if (!bet) throw new NotFoundException('error: bet not found');
+
+    return bet;
+  }
+
+  public async deleteBetByUserId(
+    userId: number,
+    betId: number,
+  ): Promise<boolean> {
+    const bet = await this.show(userId, betId);
+    const deleted = !!(await this.prisma.bet.delete({ where: { id: bet.id } }));
+    return deleted;
+  }
+
+  public async updateBetByUserId(
+    userId: number,
+    betId: number,
+    data: UpdateBetInputDTO,
+  ): Promise<Bet> {
+    const bet = await this.show(userId, betId);
+    const betUpdated = await this.prisma.bet.update({
+      data,
+      where: { id: bet.id },
+      include: { game: true, user: true },
+    });
+
+    return betUpdated;
+  }
 }
+
+type showOptions = {
+  include: any;
+};
